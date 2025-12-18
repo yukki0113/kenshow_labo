97ac9cc09745189d56dde4496956f655625e3372
yuki.i <iharacchiy@gmail.com>
2025-12-18 22:35:34 +0900

    -コミット漏れ

--- src/sql/01_TABLE/TR_RaceEntry.sql
+++ src/sql/01_TABLE/TR_RaceEntry.sql
@@ -1,34 +1,29 @@
 CREATE TABLE dbo.TR_RaceEntry
 (
-      id             INT IDENTITY(1,1) NOT NULL
-    , race_id        BIGINT           NOT NULL
-
+      id             INT IDENTITY(1,1)  NOT NULL
+    , race_id        CHAR(12)           NOT NULL
     -- レース基本情報（TR_RaceResult に揃える）
-    , 日付           DATE             NOT NULL
-    , レース名       NVARCHAR(200)    NOT NULL
-    , クラス         NVARCHAR(50)     NULL      -- G1, 3歳以上1勝クラス 等
-    , classSimple    NVARCHAR(20)     NULL      -- G1/G2/G3/OP/1勝 等のシンプル分類
-    , WIN5_flg       BIT              NULL      -- WIN5 対象レースなら 1
-
-    , 場名           NVARCHAR(50)     NOT NULL  -- 東京, 中山, 札幌 ...
-    , 芝_ダート      NVARCHAR(10)     NOT NULL  -- 芝 / ダ / 障害 など
-    , 距離           INT              NOT NULL  -- メートル
-    , 開催           NVARCHAR(50)     NULL      -- 1回東京6日目 等
-    , 回り           NVARCHAR(10)     NULL      -- 右 / 左
-    , 馬場           NVARCHAR(10)     NULL      -- 良 / 重 等（想定で入れておく）
-    , 天気           NVARCHAR(10)     NULL
-    , track_id       INT              NULL      -- 既存のID体系を踏襲
-
+    , race_date      DATE               NOT NULL
+    , race_name      NVARCHAR(50)       NOT NULL
+    , race_class     NVARCHAR(20)       NULL      -- G1, 3歳以上1勝クラス 等
+    , classSimple    NVARCHAR(20)       NULL      -- G1/G2/G3/OP/1勝 等のシンプル分類
+    , track_name     NVARCHAR(3)        NOT NULL  -- 東京, 中山, 札幌 ...
+    , surface_type   NVARCHAR(10)       NOT NULL  -- 芝 / ダ / 障害 など
+    , distance_m     INT                NOT NULL  -- メートル
+    , meeting        NVARCHAR(12)       NULL      -- 1回東京6日目 等
+    , turn           NVARCHAR(1)        NULL      -- 右 / 左
+    , going          NVARCHAR(1)        NULL      -- 良 / 重 等（想定で入れておく）
+    , weather        NVARCHAR(1)        NULL
+    , track_id       INT                NULL      -- 既存のID体系を踏襲
     -- 出走馬情報（出馬表）
-    , 枠番           INT              NOT NULL
-    , 馬番           INT              NOT NULL
-    , horse_id       BIGINT           NULL      -- 取れれば入れる、なければ NULL
-    , 馬名           NVARCHAR(100)    NOT NULL
-    , 性             NVARCHAR(10)     NULL
-    , 齢             INT              NULL
-    , 騎手           NVARCHAR(50)     NULL
-    , 斤量           DECIMAL(4,1)     NULL
-
+    , frame_no       INT                NOT NULL
+    , horse_no       INT                NOT NULL
+    , horse_id       BIGINT             NULL      -- 取れれば入れる、なければ NULL
+    , horse_name     NVARCHAR(30)       NOT NULL
+    , sex            NVARCHAR(1)        NULL
+    , age            INT                NULL
+    , jockey_name    NVARCHAR(10)       NULL
+    , carried_weight DECIMAL(3,1)       NULL
     -- 管理用（任意）
     , source_url     NVARCHAR(500)    NULL
     , scraped_at     DATETIME2(0)     NOT NULL
